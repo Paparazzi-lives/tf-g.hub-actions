9619a6cc14bdb112a246f8969fb63814b0d99b50
APPAU BISMARK <appaubis@yahoo.com>
2025-01-13 11:04:35 +0000

    lamda version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "jomaws-lambda1"
+  function_name = "jomacs-lambda1"
   description   = "My 41.6% cool lambda function"
   handler       = "index.lambda_handler"
   runtime       = "python3.12"
